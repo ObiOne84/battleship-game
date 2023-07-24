0f26dff34c6da7f5df957fe90fd80ae261caa021
ObiOne84 <>
2023-07-24 20:03:02 +0000

    Add function to print the game boards

--- run.py
+++ run.py
@@ -15,7 +15,37 @@ def print_out(data):
 
 
 class GameBoard:
+    """
+    The class holds the game board, ships position and user guesses
+    """
 
     def __init__(self, size):
         self.size = size
         self.board = [["-" for x in range(size)] for y in range(size)]
+
+    def print_board(self, size):
+        """
+        Function prints the game board, take size as parameter
+        and sets letter at the begining of each row
+        """
+
+        alphabet = ["ABCDEFGHIJKLMNOPRTQUXYZ"]
+
+        # if n is a paramter, take n number of characters from the alphabet
+        alphabet = ''.join(alphabet[:size])
+
+        column_values = []
+        for num in range(size):
+            column_values.append(num + 1)
+
+        print(" ", *column_values)
+
+# loop over the zipped lists, row is the index, cell is the list in the loop
+        for index, row in enumerate(zip(alphabet, self.board)):
+            # there are two items in the list,
+            # row[0] is the alphabet character, row[1] is the board row
+            # the character can be printed as is
+            # the board list must be joined as a string
+            print(
+                f'{row[0]} ', ' '.join(x for x in row[1])
+            )
