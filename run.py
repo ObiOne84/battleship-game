45f9d454d0b4eb26f002f38dd451517da86ad829
ObiOne84 <>
2023-07-24 19:46:34 +0000

    Add function to print out messages for user

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 import time
 import sys
 from random import randint
+
+
+def print_out(data):
+    """
+    The function will display text as it typed in real
+    time rather than dispaly all message at once
+    """
+    for letter in data:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.05)
