a4ed546353a3601ee79f9f9230c6b70d7ebff392
ObiOne84 <>
2023-07-24 19:49:17 +0000

    Add class GameBoard and define board

--- run.py
+++ run.py
@@ -12,3 +12,10 @@ def print_out(data):
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(0.05)
+
+
+class GameBoard:
+
+    def __init__(self, size):
+        self.size = size
+        self.board = [["-" for x in range(size)] for y in range(size)]
