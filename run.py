18769c6c5fc5daf6dba268696cadc454888ccbe5
ObiOne84 <>
2023-08-05 11:42:01 +0000

    Update main function, uncomment logo and intro

--- run.py
+++ run.py
@@ -711,8 +711,8 @@ def main():
     Functions controls entire game, by calling all functions
     """
     while True:
-        # game_logo()
-        # game_intro()
+        game_logo()
+        game_intro()
         print("Would you like to start the game?")
         print_out("Choose 'Y' to start the game or 'N' to leave now.\n")
         decision = start_game()
